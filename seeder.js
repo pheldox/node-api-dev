1aba45dcae1f24993953281f0c53a9a67e855883
pheldox <felixaryee4@gmail.com>
2020-03-27 11:47:22 +0000

    added course model

--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: './config/config.env' });
 
 // Load models
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 
 // connect to db
 
@@ -23,13 +24,18 @@ const bootcamps = JSON.parse(
 	fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
 
+const courses = JSON.parse(
+	fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
+
 // Import into DB
 const importData = async () => {
 	try {
 		await Bootcamp.create(bootcamps);
+		await Course.create(courses);
 		console.log('Data Imported....'.green.inverse);
 		process.exit();
-	} catch (error) {
+	} catch (err) {
 		console.error(err);
 	}
 };
@@ -38,10 +44,11 @@ const importData = async () => {
 const deleteData = async () => {
 	try {
 		await Bootcamp.deleteMany();
+		await Course.deleteMany();
 
 		console.log('Data deleted...'.red.inverse);
 		process.exit();
-	} catch (error) {
+	} catch (err) {
 		console.error(err);
 	}
 };
