32cfe1d10845501dbca545f05a3a952f1b46d598
pheldox <felixaryee4@gmail.com>
2020-03-28 00:00:55 +0000

    get courses for a bootcamp

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const connectDB = require('./config/db');
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
-
 connectDB();
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
+const courses = require('./routes/courses');
 
 const app = express();
 
@@ -20,24 +20,27 @@ const app = express();
 app.use(express.json());
 
 //Dev loggin middleware
-if(process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
 }
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
 
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
-  PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+	PORT,
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+	)
 );
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
-  // Close server  exit process
-  server.close(() => process.exit(1));
+	console.log(`Error: ${err.message}`);
+	// Close server  exit process
+	server.close(() => process.exit(1));
 });
