99938de03a588137026201eedb7273ac4982a5e7
pheldox <felixaryee4@gmail.com>
2020-03-29 11:34:17 +0000

    create auth routes and controllers

---
+++ controllers/auth.js
@@ -0,0 +1,11 @@
+const ErrorResponse = require('../utils/errorResponse');
+const User = require('../models/User');
+const asyncHandler = require('../middleware/async');
+
+// @desc      Register
+// @route     GET /api/v1/auth
+// @access    Public
+
+exports.register = asyncHandler(async (req, res, next) => {
+	res.status(200).json({ success: true });
+});
