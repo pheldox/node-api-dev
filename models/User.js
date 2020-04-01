bea3c858c1ae0e38c0713873a61f7629dba10d5a
pheldox <felixaryee4@gmail.com>
2020-04-01 15:48:12 +0000

    forgot password

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
@@ -36,6 +37,9 @@ const UserSchema = new mongoose.Schema({
 
 // Encypt password using bcrypt
 UserSchema.pre('save', async function(next) {
+	if (!this.isModified('password')) {
+		next();
+	}
 	const salt = await bcrypt.genSalt(10);
 	this.password = await bcrypt.hash(this.password, salt);
 });
@@ -50,4 +54,21 @@ UserSchema.methods.getSignedJwtToken = function(next) {
 UserSchema.methods.matchPassword = async function(enteredPassword) {
 	return await bcrypt.compare(enteredPassword, this.password);
 };
+
+// Generate and hash password token
+UserSchema.methods.getResetPasswordToken = function() {
+	// Generate token
+	const resetToken = crypto.randomBytes(20).toString('hex');
+
+	// Hash token and set to to resetPasswordToken field
+	this.resetPasswordToken = crypto
+		.createHash('sha256')
+		.update(resetToken)
+		.digest('hex');
+
+	// Set exprie
+	this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+	return resetToken;
+};
+
 module.exports = mongoose.model('User', UserSchema);
