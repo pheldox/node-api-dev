b54b726f66fa11250eef40d76450021f7740dc01
pheldox <felixaryee4@gmail.com>
2020-04-02 15:23:08 +0000

    update user details when forget password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse');
 const User = require('../models/User');
 const asyncHandler = require('../middleware/async');
+const sendEmail = require('../utils/sendEmail');
 
 // @desc      Register
 // @route     GET /api/v1/auth/register
@@ -79,10 +80,31 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
 
 	await user.save({ validateBeforeSave: false });
 
-	res.status(200).json({
-		success: true,
-		data: user
-	});
+	// Create reset url
+	const resetUrl = `${req.protocol}://${req.get(
+		'host'
+	)}//api/v1/resetpassword/${resetToken}`;
+
+	const message = `You are receiving this email because you ( or someone else) has
+	 requested te reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
+
+	try {
+		await sendEmail({
+			email: user.email,
+			subject: 'password reset token',
+			message
+		});
+
+		res.status(200).json({ success: true, data: 'Email sent' });
+	} catch (error) {
+		console.log(error);
+		user.getResetPasswordToken = undefined;
+		user.resetPasswordExpire = undefined;
+
+		await user.save({ validateBeforeSave: fasle });
+
+		return next(new ErrorResponse('Email could not be sent', 500));
+	}
 });
 
 // Get token from model, create cookie and send response
