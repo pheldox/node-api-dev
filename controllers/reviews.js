b6c889a9776505f64179ac5f0c8f9e4e39ebe2c3
pheldox <felixaryee4@gmail.com>
2020-04-03 22:55:46 +0000

    update review

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -58,7 +58,7 @@ exports.addReview = asyncHandler(async (req, res, next) => {
 
 	const bootcamp = await Bootcamp.findById(req.params.bootcampId);
 	if (!bootcamp) {
-		return nexr(
+		return next(
 			new ErrorResponse(
 				`Not bootcamp with the id of ${req.params.bootcampId}`,
 				404
@@ -70,5 +70,56 @@ exports.addReview = asyncHandler(async (req, res, next) => {
 	res.status(200).json({
 		success: true,
 		data: review
+	});
+});
+
+// @desc      Update a  review
+// @route     POSTT /api/v1/reviews/:id
+// @route GET /api/v1/bootcamps/:bootcampId/reviews
+// @access    Private
+
+exports.updateReview = asyncHandler(async (req, res, next) => {
+	let review = await Review.findById(req.params.id);
+	if (!review) {
+		return next(
+			new ErrorResponse(`Not review with the id of ${req.params.id}`, 404)
+		);
+	}
+
+	// Make sure review belongs to usr or user is admin
+	if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+		return next(new ErrorResponse(`Not authorized to update review`, 401));
+	}
+	review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+		runValidators: true
+	});
+	res.status(201).json({
+		success: true,
+		data: review
+	});
+});
+
+// @desc      Update a  review
+// @route     POSTT /api/v1/reviews/:id
+// @route GET /api/v1/bootcamps/:bootcampId/reviews
+// @access    Private
+
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+	const review = await Review.findById(req.params.id);
+	if (!review) {
+		return next(
+			new ErrorResponse(`Not review with the id of ${req.params.id}`, 404)
+		);
+	}
+
+	// Make sure review belongs to usr or user is admin
+	if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+		return next(new ErrorResponse(`Not authorized to update review`, 401));
+	}
+	await review.remove();
+	res.status(201).json({
+		success: true,
+		data: {}
 	});
 });
